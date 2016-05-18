fd8a29f69f2fc34eb6e084138cff4c0b2aa99d5e
ryonext <ryonext.s@gmail.com>
2016-05-18 11:09:44 +0900

    Refactoring sessions_controller.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,29 +3,21 @@ class SessionsController < ApplicationController
   end
 
   def create
-    client = TwitterOAuth::Client.new(
-      consumer_key: ENV["CONSUMER_KEY"],
-      consumer_secret: ENV["CONSUMER_SECRET"],
-    )
-    request_token = client.request_token(oauth_callback: callback_url)
+    request_token = oauth_client.request_token(oauth_callback: callback_url)
     session[:request_token] = request_token.token
     session[:request_token_secret] = request_token.secret
     redirect_to request_token.authorize_url
   end
 
   def callback
-    client = TwitterOAuth::Client.new(
-      consumer_key: ENV["CONSUMER_KEY"],
-      consumer_secret: ENV["CONSUMER_SECRET"],
-    )
-    access_token = client.authorize(
+    access_token = oauth_client.authorize(
       session[:request_token],
       session[:request_token_secret],
       oauth_verifier: params[:oauth_verifier],
     )
     session[:access_token] = access_token.token
     session[:access_token_secret] = access_token.secret
-    redirect_to "/"
+    redirect_to root_path
   end
 
   private
@@ -33,5 +25,12 @@ class SessionsController < ApplicationController
     def callback_url
       host = ENV["HOST"] || "http://localhost:3000"
       File.join(host, "/session/callback")
+    end
+
+    def oauth_client
+      @oauth_client ||= TwitterOAuth::Client.new(
+        consumer_key: ENV["CONSUMER_KEY"],
+        consumer_secret: ENV["CONSUMER_SECRET"],
+      )
     end
 end
